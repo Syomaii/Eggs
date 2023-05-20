0c1f2dd7f85e04ff55de2bc64cd484537374be0e
Syomai <126589688+Syomaii@users.noreply.github.com>
2023-05-20 17:38:39 +0800

    Add files via upload

---
+++ Eggs/Eggs/Egg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eggs
+{
+    internal class Egg
+    {
+        FrontTuyok f = new FrontTuyok();
+        BackTuyok b = new BackTuyok();
+        private EggType eggType;
+
+        public Egg(EggType eggType)
+        {
+            this.eggType = eggType;
+        }
+        public void displayinfo()
+        {
+            Console.WriteLine("***Pagbali sa Egg***");
+            f.tuyok();
+            b.tuyok();
+            Console.WriteLine($"You have a {eggType} order");
+        }
+    }
+}
---
+++ Eggs/Eggs/FrontTuyok.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eggs
+{
+    internal class FrontTuyok:Tuyok 
+    {
+        public void tuyok()
+        {
+            Console.WriteLine("Atubangan: White and Yellow Color");
+        }
+    }
+}
